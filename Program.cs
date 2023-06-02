d74fc15194a0141aa865453df6454d2b037952a8
Bournane Mohamed <mbournane@gmail.com>
2023-06-02 02:07:33 +0200

    Atelier Boucle

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Boucle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] moves = new string[32];
+        int i = 0;
+        while(true)
+        {
+            Console.Write("veuillez entrer un pas de danse: ");
+            string unPas = Console.ReadLine();
+            if(String.IsNullOrEmpty(unPas))
+            {
+                continue;
+            }else{
+                moves[i] = unPas;
+
+            }
+
+            if(unPas == "done")
+                break;
+
+            i++;
+            
+
+        }
+        Console.Write("Saisissez un nombre d'itérations de la suite de pas: ");
+        int nbIteration =Int32.Parse(Console.ReadLine());
+        for(int j =0; j< nbIteration;j++)
+        {
+            int k = 0;
+
+            while(moves[k] != "done" || k >=32)
+            {
+                Console.Write(moves[k] + " ");
+                k++;
+            }
+            Console.WriteLine(" ");
+        }
+
+      
+        
+    }
+}
